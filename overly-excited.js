4f170b8314f4c2b00e8a9c4f89665669f7813bbc
Corri Golden <corri.golden@gmail.com>
2020-04-11 18:40:23 -0500

    finished

--- overly-excited.js
+++ overly-excited.js
@@ -1,28 +1,32 @@
 console.log("Hello, world!")
 
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
 
-function addExcitement (theWordArray) {
+addExcitement = (theWordArray, punc) => {
 
-        // Each time the for loop executes, you're going to add one more word to this string
-        let buildMeUp = ""
-    
-        for (let i = 0; i < theWordArray.length; i++) {
-            console.log(buildMeUp += theWordArray[i] + " ")
-            // Concatenate the new word onto buildMeUp
-            // Print buildMeUp to the console
-            
-         if (i % 3 == 2){
-            console.log(buildMeUp += "?");
-        } 
-        }
-    }
- //addExcitement(sentence), "?";
-    // buildMeUp += theWordArray[i] + " "
-// I want to use a question mark
- addExcitement(sentence), "?";
+        // Each time the for loop executes, you're going to add one more word to this string
+        let buildMeUp = ""
+    
+        for (let i = 0; i < theWordArray.length; i++) {
+            console.log(buildMeUp += theWordArray[i] + " ")
+            // Concatenate the new word onto buildMeUp
+            // Print buildMeUp to the console
+            
+         if(i % 3 == 2){
+            buildMeUp += punc + " "
+            // console.log(buildMeUp += punc + " ");
+            console.log(buildMeUp)
+        } 
+        }
+    }
+ //addExcitement(sentence), "?";
+    // buildMeUp += theWordArray[i] + " "
+// I want to use a question mark
+ addExcitement(sentence, "!!!");
 
- //else {
-    //console.log(buildMeUp += theWordArray[i] + "?");
+
+
+ //else {
+    //console.log(buildMeUp += theWordArray[i] + "?");
 
